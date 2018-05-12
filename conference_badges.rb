84b5a73977d6d5bddd7447f55a559b90768841e8
Taylor Williams <taylor@thewilliams.net>
2018-05-12 17:56:46 +0000

    automatically backed up by learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -8,3 +8,9 @@ def batch_badge_creator(names_array)
     names_array[index] = badge_maker(name)
   end
 end
+
+def assign_rooms(names_array)
+  names_array.each_with_index do |name, index|
+    names_array[index] = "Hello, #{name}! You'll be assigned to room #{index}!"
+  end
+end
